cbaa45c21b9ebc7f314f6b8dec257de6664e52e6
thibault finot <thibault.finot@gmail.com>
2015-12-29 18:40:50 +0100

    picture dandom with unsplash violent ok

--- app/helpers/projets_helper.rb
+++ app/helpers/projets_helper.rb
@@ -1,2 +1,9 @@
 module ProjetsHelper
+  def picture_special
+    ['https://unsplash.it/800/250', 'https://unsplash.it/800/250/?image=0', 'https://unsplash.it/400/250/?image=13', 'https://unsplash.it/400/250/?image=50', 'https://unsplash.it/400/250/?image=230'].sample
+  end
+
+  def picture_thumb
+    ['https://unsplash.it/800/600', 'https://unsplash.it/800/600/?image=0', 'https://unsplash.it/800/600/?image=13', 'https://unsplash.it/800/600/?image=50', 'https://unsplash.it/800/600/?image=230'].sample
+  end
 end
