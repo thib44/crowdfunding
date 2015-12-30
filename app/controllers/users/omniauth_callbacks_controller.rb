703f8aade970a7f712bd03f20be78296399af8b1
thibault finot <thibault.finot@gmail.com>
2015-12-30 14:11:01 +0100

    refacto code

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,24 +1,23 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
     user = User.find_for_facebook_oauth(request.env['omniauth.auth'])
-
-    if user.persisted?
-      sign_in_and_redirect user, event: :authentication
-      set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
-    else
-      session['devise.facebook_data'] = request.env['omniauth.auth']
-      redirect_to new_user_registration_url
-    end
+    strategie(user, "Facebook")
   end
 
-    def linkedin
+  def linkedin
     user = User.find_for_linkedin_oauth(request.env['omniauth.auth'])
+    strategie(user, "Linkedin")
+  end
 
+
+  private
+
+  def strategie(user, kind)
     if user.persisted?
       sign_in_and_redirect user, event: :authentication
-      set_flash_message(:notice, :success, kind: 'Linkedin') if is_navigational_format?
+      set_flash_message(:notice, :success, kind: kind) if is_navigational_format?
     else
-      session['devise.linkedin_data'] = request.env['omniauth.auth']
+      session["devise.#{kind.downcase}_data"] = request.env['omniauth.auth']
       redirect_to new_user_registration_url
     end
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,20 +10,19 @@ class User < ActiveRecord::Base
 
 
   def self.find_for_facebook_oauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.provider = auth.provider
-      user.uid = auth.uid
-      user.email = auth.info.email
-      user.password = Devise.friendly_token[0,20]  # Fake password for validation
-      user.first_name = auth.info.first_name
-      user.last_name = auth.info.last_name
-      user.picture = auth.info.image
-      user.token = auth.credentials.token
-      user.token_expiry = Time.at(auth.credentials.expires_at)
-    end
+    user = omniauth_strategie(auth)
+    user.token_expiry = Time.at(auth.credentials.expires_at)
+    user
   end
 
   def self.find_for_linkedin_oauth(auth)
+    omniauth_strategie(auth)
+    # user.token_expiry = Time.at(auth.credentials.expires_at)
+  end
+
+  private
+
+  def omniauth_strategie(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.provider = auth.provider
       user.uid = auth.uid
@@ -33,7 +32,6 @@ class User < ActiveRecord::Base
       user.last_name = auth.info.last_name
       user.picture = auth.info.image
       user.token = auth.credentials.token
-      # user.token_expiry = Time.at(auth.credentials.expires_at)
     end
   end
 
