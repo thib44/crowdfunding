ba6250943fede9e5e0f640acd9227ceefa8715c8
thibault finot <thibault.finot@gmail.com>
2015-12-30 10:49:12 +0100

    cgu

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ Rails.application.routes.draw do
     root to: 'users#index', as: :authenticated_root
   end
   root 'pages#home'
+
+  get 'cgu' => 'pages#cgu'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
